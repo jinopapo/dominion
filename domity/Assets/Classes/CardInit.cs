c602bfa26bd654be1fe5ccce095c773133bdea00
nezawa atsuhiro <ji-no@nezawa-no-MacBook-Air.local>
2015-12-29 20:55:05 +0900

    add domity

---
+++ domity/Assets/Classes/CardInit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+public class CardInit {
+	public int purchase;
+	public int cost;
+	public int money;
+	public int action;
+	public int draw;
+	public int vpoint;
+	public bool isMoney;
+	public bool isVpoint;
+	public bool isAction;
+}
---
+++ domity/Assets/Script/Mine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour{
+  Card card;
+
+  public void Action(){
+    int cost = card.Owner.GetComponent<Player>().SelectedRemove();
+    card.Owner.GetComponent<Player>().SetGetParam(cost+3,isM:true,hand:true);
+  }
+
+  public bool EndAction(){
+    if (card.Owner.GetComponent<Player>().CountSelected() > 1 && card.Owner.GetComponent<Player>().IsSelectedMoney())
+      return false;
+    Action();
+    card.EndEvent ();
+    return true;
+  }
+
+	// Use this for initialization
+	void Start () {
+    card = gameObject.GetComponent<Card>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (card.Event){
+      card.Owner.GetComponent<Player>().selectMode = true;
+    }
+	}
+}
